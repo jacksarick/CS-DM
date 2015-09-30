dc9030cbfda3f545441b5dc55e7fa97f9bae4691
Jackie <jacksarick@gmail.com>
2015-09-30 12:27:42 -0400

    Fully annotated

--- Problem_3_Rovarspraket_WIP.py
+++ Problem_3_Rovarspraket_WIP.py
@@ -1,25 +1,30 @@
+# Teach the computer the alphabet
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 vowels = ["a", "e", "i", "o", "u"]
 vowelnum = [0, 4, 8, 14, 20]
 
+# Get the word we need, and set up a blank
 word = list(raw_input().lower())
 newword = ""
 
-def next_vowel(letter):
-	global alphabet
-	global vowels
-	global vowelnum
-
-	closest = [abs(alphabet.index(letter) - val) for val in vowelnum]
-	return alphabet[vowelnum[closest.index(min(closest))]]
-
+# Loop through the word
 for letter in word:
+	# vowels are easy
 	if letter in vowels:
+		# just pass the letter back
 		newword += letter
 
+	# we have more trouble if it's a consonant
 	else:
+		# add our original letter
 		newword += letter
-		newword += next_vowel(letter)
+
+		# this is a wicked confusing thing that finds the closest vowel index
+		closest = [abs(alphabet.index(letter) - val) for val in vowelnum]
+		# converts vowel index to actual vowel
+		newword += next_vowel(alphabet[vowelnum[closest.index(min(closest))]])
+
+		# this just adds the next letter in the alphabet
 		newword += alphabet[alphabet.index(letter) + 1]
 
 print newword
