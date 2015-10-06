f8027a1024a5ebf55709404c5e06483712bfc280
Jackie <jacksarick@gmail.com>
2015-10-06 13:05:37 -0400

    update

--- FizzBuzz/betterbuzz.py
+++ FizzBuzz/betterbuzz.py
@@ -1,2 +1,9 @@
+# find the sum of a number
 def sum_number(number):
-	
+	numbers = list(str(number))
+	numbers = [int(character) for character in numbers]
+	sum_of_numbers = sum(numbers)
+	return sum_of_numbers
+
+for i in range(1,100):
+	pass
