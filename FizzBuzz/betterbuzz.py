86f60b1cd84c0ef4b9a88477b54bb7f2584f6c54
Jackie <jacksarick@gmail.com>
2015-10-06 13:09:34 -0400

    Done! BetterBuzz

--- FizzBuzz/betterbuzz.py
+++ FizzBuzz/betterbuzz.py
@@ -5,5 +5,15 @@ def sum_number(number):
 	sum_of_numbers = sum(numbers)
 	return sum_of_numbers
 
-for i in range(1,100):
-	pass
+for number in range(1,100):
+	if (sum_number(number) % 3 == 0) and (str(number)[-1] == "0" or str(number)[-1] == "5"):
+		print "FizzBuzz"
+
+	if (sum_number(number) % 3 == 0):
+		print "Fizz"
+
+	if (str(number)[-1] == "0" or str(number)[-1] == "5"):
+		print "Buzz"
+		
+	else:
+		print number
